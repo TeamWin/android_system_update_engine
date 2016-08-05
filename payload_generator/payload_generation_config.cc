5b91c6b141970c2b0095775a61e3f941417aa1ff
Alex Deymo <deymo@google.com>
2016-08-04 20:33:36 -0700

    New setting to mark postinstall as optional.
    This setting allows to mark a postinstall script as optional. This
    allows the postinstall program to fail when it is not strictly required
    to run for the update to succeed.
    
    Bug: 27178350
    TEST=Added unittest. Sideloaded an update with an optional postinstall.
    
    Change-Id: I41956d3308f3458b6bf94b835c9b5e470c84ca41

--- payload_generator/payload_generation_config.cc
+++ payload_generator/payload_generation_config.cc
@@ -28,7 +28,7 @@
 namespace chromeos_update_engine {
 
 bool PostInstallConfig::IsEmpty() const {
-  return run == false && path.empty() && filesystem_type.empty();
+  return !run && path.empty() && filesystem_type.empty() && !optional;
 }
 
 bool PartitionConfig::ValidateExists() const {
@@ -90,6 +90,8 @@ bool ImageConfig::LoadPostInstallConfig(const brillo::KeyValueStore& store) {
     store.GetString("POSTINSTALL_PATH_" + part.name, &part.postinstall.path);
     store.GetString("FILESYSTEM_TYPE_" + part.name,
                     &part.postinstall.filesystem_type);
+    store.GetBoolean("POSTINSTALL_OPTIONAL_" + part.name,
+                     &part.postinstall.optional);
   }
   if (!found_postinstall) {
     LOG(ERROR) << "No valid postinstall config found.";
