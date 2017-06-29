8c3c80cbc706137fa1a3dc8784fd8108c0841b30
Sen Jiang <senj@google.com>
2017-06-28 17:13:19 -0700

    Add maximum timestamp to the payload.
    Added a new field max_timestamp in the protobuf, from now on
    update_engine will reject any payload without this field.
    If the OS build timestamp is newer than the max_timestamp, the payload
    will also be rejected to prevent downgrade.
    
    Bug: 36232423
    Test: update_engine_unittests
    
    Merged-In: Ib20f5f35aaf41165013bada02bc8720917358237
    Change-Id: Ib20f5f35aaf41165013bada02bc8720917358237
    (cherry picked from commit 5011df680621eb477cad8b34f03fba5b542cc2f9)
    (cherry picked from commit ccb01b2a3beb94094388de806bc15b210ebe8b11)

--- common/fake_hardware.h
+++ common/fake_hardware.h
@@ -81,6 +81,8 @@ class FakeHardware : public HardwareInterface {
     return false;
   }
 
+  int64_t GetBuildTimestamp() const override { return build_timestamp_; }
+
   // Setters
   void SetIsOfficialBuild(bool is_official_build) {
     is_official_build_ = is_official_build;
@@ -125,6 +127,10 @@ class FakeHardware : public HardwareInterface {
     powerwash_count_ = powerwash_count;
   }
 
+  void SetBuildTimestamp(int64_t build_timestamp) {
+    build_timestamp_ = build_timestamp;
+  }
+
  private:
   bool is_official_build_{true};
   bool is_normal_boot_mode_{true};
@@ -137,6 +143,7 @@ class FakeHardware : public HardwareInterface {
   std::string ec_version_{"Fake EC v1.0a"};
   int powerwash_count_{kPowerwashCountNotSet};
   bool powerwash_scheduled_{false};
+  int64_t build_timestamp_{0};
 
   DISALLOW_COPY_AND_ASSIGN(FakeHardware);
 };
