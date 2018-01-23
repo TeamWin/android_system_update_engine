2a16ed0ab8261a8a7f5e664c1314017edbacc10c
Eric Caruso <ejcaruso@chromium.org>
2018-01-23 14:40:27 -0800

    update_engine: qualify stl_util functions
    This uses ContainsKey from stl_util which has been moved to the
    base namespace in future versions of libchrome.
    
    BUG=b:37434548
    TEST=emerge, run unit tests
    
    Change-Id: Id81f7ba125ce99f5a95af317ecfa8910e740dd22
    Reviewed-on: https://chromium-review.googlesource.com/882383
    Commit-Ready: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Amin Hassani <ahassani@chromium.org>

--- connection_manager.cc
+++ connection_manager.cc
@@ -86,7 +86,7 @@ bool ConnectionManager::IsUpdateAllowedOver(
       if (device_policy->GetAllowedConnectionTypesForUpdate(&allowed_types)) {
         // The update setting is enforced by the device policy.
 
-        if (!ContainsKey(allowed_types, shill::kTypeCellular)) {
+        if (!base::ContainsKey(allowed_types, shill::kTypeCellular)) {
           LOG(INFO) << "Disabling updates over cellular per device policy.";
           return false;
         }
