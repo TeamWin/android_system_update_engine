0d78f45ff5330b01581092c207be961c0ccb6926
Gabriele M <moto.falcon.git@gmail.com>
2018-06-14 01:10:09 +0200

    update_engine: Add performance mode
    Allow to move update_engine from the system-background cgroup
    to the foreground cgroup to speed up the installation of the
    updates.
    
    Change-Id: Iaa531a925f9e1a26e834d7448c4755151adcfea2

--- update_attempter_android.cc
+++ update_attempter_android.cc
@@ -29,6 +29,7 @@
 #include <brillo/data_encoding.h>
 #include <brillo/message_loops/message_loop.h>
 #include <brillo/strings/string_utils.h>
+#include <cutils/sched_policy.h>
 #include <log/log_safetynet.h>
 
 #include "update_engine/common/constants.h"
@@ -109,6 +110,7 @@ UpdateAttempterAndroid::UpdateAttempterAndroid(
       clock_(new Clock()) {
   metrics_reporter_ = metrics::CreateMetricsReporter();
   network_selector_ = network::CreateNetworkSelector();
+  set_cpuset_policy(0, SP_BACKGROUND);
 }
 
 UpdateAttempterAndroid::~UpdateAttempterAndroid() {
@@ -420,6 +422,18 @@ bool UpdateAttempterAndroid::VerifyPayloadApplicable(
     }
     fd->Close();
   }
+  return true;
+}
+
+bool UpdateAttempterAndroid::SetPerformanceMode(bool enable,
+                                                brillo::ErrorPtr* error) {
+  LOG(INFO) << (enable ? "Enabling" : "Disabling") << " performance mode.";
+
+  if (performance_mode_ == enable)
+    return true;
+  if (set_cpuset_policy(0, enable ? SP_FOREGROUND : SP_BACKGROUND) < 0)
+    return LogAndSetError(error, FROM_HERE, "Could not change policy");
+  performance_mode_ = enable;
   return true;
 }
 
