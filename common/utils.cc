01c623158e67f1a97070173f7ac229c34af25b39
Captain Throwback <captainthrowback@hotmail.com>
2022-04-23 20:32:36 -0400

    utils.cc: remove kPayloadTimestampError check
    Change-Id: I894c680f5d179b715cd3e4330045d81a40f21732

--- common/utils.cc
+++ common/utils.cc
@@ -1194,9 +1194,6 @@ ErrorCode IsTimestampNewer(const std::string_view old_version,
   if (!ParseTimestamp(new_version, &new_ver)) {
     return ErrorCode::kDownloadManifestParseError;
   }
-  if (old_ver > new_ver) {
-    return ErrorCode::kPayloadTimestampError;
-  }
   return ErrorCode::kSuccess;
 }
 
