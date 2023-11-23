57cc651258206102a2296e891729c224ca0974d7
twinnfamous <twinnfamous@gmail.com>
2023-11-23 12:27:26 -0800

    Allow empty version on partial update & Bypass checks for missing partitions in payload
    Change-Id: If66c0e13bd79675279fde0626c46181feb2205fa

--- payload_consumer/delta_performer.cc
+++ payload_consumer/delta_performer.cc
@@ -662,12 +662,7 @@ bool DeltaPerformer::ParseManifestPartitions(ErrorCode* error) {
     auto generator = partition_update_generator::Create(boot_control_,
                                                         manifest_.block_size());
     std::vector<PartitionUpdate> untouched_static_partitions;
-    TEST_AND_RETURN_FALSE(
-        generator->GenerateOperationsForPartitionsNotInPayload(
-            install_plan_->source_slot,
-            install_plan_->target_slot,
-            touched_partitions,
-            &untouched_static_partitions));
+
     partitions_.insert(partitions_.end(),
                        untouched_static_partitions.begin(),
                        untouched_static_partitions.end());
@@ -1156,7 +1151,7 @@ ErrorCode DeltaPerformer::CheckTimestampError() const {
     // But max_timestamp can be empty
     for (const auto& partition : partitions) {
       auto error_code = timestamp_valid(
-          partition, false /* allow_empty_version */, &downgrade_detected);
+          partition, true /* allow_empty_version */, &downgrade_detected);
       if (error_code != ErrorCode::kSuccess &&
           error_code != ErrorCode::kPayloadTimestampError) {
         return error_code;
